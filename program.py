4d90debfc143b3e4bc53424540f7c36670a5bd2d
alexisabena <32970506+alexisabena@users.noreply.github.com>
2022-02-15 22:21:14 -0600

    Programas de Python

---
+++ program.py
@@ -0,0 +1,24 @@
+# program.py
+from datetime import date
+
+
+sum = 1 + 2
+print (sum)
+
+left_side = 10
+right_side = 5
+print(left_side / right_side)
+
+date.today()
+print(date.today())
+
+print("Today's date is: " + str(date.today()))
+
+print("Bienvenido al programa de bienvenida")
+name = input("Introduce tu nombre ")
+print("Saludos: " + name)
+
+print("Calculadora")
+first_number = input("Primer número: ")
+second_number = input("Segundo número: ")
+print(first_number + second_number)
